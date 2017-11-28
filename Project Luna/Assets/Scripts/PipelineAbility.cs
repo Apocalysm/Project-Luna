e7f274c6d4874daa354ac8271ebefa102e994113
UTB\andera <apocalysm1@gmail.com>
2017-11-28 15:18:13 +0100

    Started working with pipeline
    only started small

--- Project Luna/Assets/Scripts/PipelineAbility.cs
+++ Project Luna/Assets/Scripts/PipelineAbility.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PipelineAbility : MonoBehaviour {
+public class PipelineAbility : MonoBehaviour
+{
+    List<bool> ability = new List<bool>();
+    List<KeyCode> keys = new List<KeyCode>();
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
