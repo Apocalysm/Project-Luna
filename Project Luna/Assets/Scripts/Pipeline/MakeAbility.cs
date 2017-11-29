5da5c375d9150c28914a4ca3602be710dd58080f
UTB\andera <apocalysm1@gmail.com>
2017-11-29 14:22:47 +0100

    Pipeline
    Working with pipeline

---
+++ Project Luna/Assets/Scripts/Pipeline/MakeAbility.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class MakeAbility
+{
+
+    [MenuItem("Assets/Scripts/Ability")]
+    public static void CreateMyAsset()
+    {
+        Ability asset = ScriptableObject.CreateInstance<Ability>();
+
+
+        AssetDatabase.CreateAsset(asset, "Assets/Scripts/NewAbility.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
+}
---
+++ Project Luna/Assets/Scripts/Pipeline/PipelineAbility.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PipeLineValue 
+{
+    [HideInInspector]
+    public bool owned = false;
+    public float coolDown;
+    public string quest;
+    public KeyCode key;
+
+    public Ability ability;
+}
+
+public class PipelineAbility : MonoBehaviour
+{
+    //Dictionary<KeyCode, PipeLineValue> values = new Dictionary<KeyCode, PipeLineValue>();
+    public PipeLineValue[] value;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {	
+    }
+}
