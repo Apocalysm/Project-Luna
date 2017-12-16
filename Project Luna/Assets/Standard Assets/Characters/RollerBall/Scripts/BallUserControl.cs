25679f98a503931b692c09cd35bda505974d8d51
Steerbjorn <nilleboyswe@gmail.com>
2017-12-16 18:45:22 +0100

    water...
    add water, trees...

--- Project Luna/Assets/Standard Assets/Characters/RollerBall/Scripts/BallUserControl.cs
+++ Project Luna/Assets/Standard Assets/Characters/RollerBall/Scripts/BallUserControl.cs
@@ -16,7 +16,24 @@ namespace UnityStandardAssets.Vehicles.Ball
         private bool jump; // whether the jump button is currently pressed
 
 
-       
+        private void Awake()
+        {
+            // Set up the reference.
+            ball = GetComponent<Ball>();
+
+
+            // get the transform of the main camera
+            if (Camera.main != null)
+            {
+                cam = Camera.main.transform;
+            }
+            else
+            {
+                Debug.LogWarning(
+                    "Warning: no main camera found. Ball needs a Camera tagged \"MainCamera\", for camera-relative controls.");
+                // we use world-relative controls in this case, which may not be what the user wants, but hey, we warned them!
+            }
+        }
 
 
         private void Update()
